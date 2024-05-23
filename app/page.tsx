738ea9d73e004cdedfca057b9288bfc6cf013b8f
sammy-coding <samuelodebunmi1@gmail.com>
2024-05-23 15:35:58 +0100

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
-'use client'
-import { I18nProvider } from "@react-aria/i18n";
+'use client';
 import { DatePicker } from '@nextui-org/date-picker';
+import { NextUIProvider } from '@nextui-org/system';
 
 export default function Home() {
-  return (
-    <I18nProvider locale="ar-SA">
-      <DatePicker lang='ar' showMonthAndYearPickers={true} aria-label="Date (International Calendar)" />
-    </I18nProvider>
-  );
+	return (
+		<NextUIProvider locale="ar-SA">
+			<DatePicker
+				showMonthAndYearPickers={true}
+				aria-label="Date (International Calendar)"
+			/>
+		</NextUIProvider>
+	);
 }
