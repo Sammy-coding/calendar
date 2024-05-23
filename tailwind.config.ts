a290733f8bebe5508740dbad4f10b6581d00ff1d
sammy-coding <samuelodebunmi1@gmail.com>
2024-05-23 12:56:48 +0100

    calendar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,10 @@
-import type { Config } from "tailwindcss";
-
+import type { Config } from 'tailwindcss';
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+  theme: {},
+  plugins: [require('@nextui-org/theme')],
 };
 export default config;
